4a7a08a115f086c429c7cbab1d6434bc74fbe0b4
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-04 23:21:15 +0200

    Modified existing views, added Gii generated views, modified them also

---
+++ views/admin/view.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Admin */
+
+$this->title = $model->first_name." ".$model->last_name;
+$this->params['breadcrumbs'][] = ['label' => 'Admin', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="admin-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Izbriši', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Jeste li sigurni da želite izbrisati admina?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'first_name',
+            'last_name',
+            'email:email',
+            'active',
+        ],
+    ]) ?>
+
+</div>
