22615064866c02eff1980f851ae68542078711f0
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-04 13:40:12 +0200

    Added rules and changed table name

--- models/Joke.php
+++ models/Joke.php
@@ -10,16 +10,25 @@ class Joke extends ActiveRecord{
     public function rules()
     {
         return [
-            [[ 'title','text'],'required']
+            [[ 'title','text','submitter'],'required'],
+            [ 'status_id','default','value'=>1],
+            ['submit_date','default','value'=>function ($model, $attribute) {
+                return date('Y-m-d H:i:s');
+            }],
+            [[ 'approval_date','admin_id','joke_of_day_date','rating'],'safe'],
+            [[ 'submit_date','approval_date'],'datetime'],
+            [[ 'joke_of_day_date'],'date'],
+            [[ 'status_id','admin_id'],'integer'],
+            [[ 'rating'],'double']
         ];
     }
     
     public static function tableName(){
-        return '{{vicevi}}';
+        return '{{joke}}';
     }
     
     public function getCategories() {
-    return $this->hasMany(Category::className(), ['id'=> 'category_id' ])->viaTable('vicevi_kategorije', ['vic_id' => 'id']);
+    return $this->hasMany(Category::className(), ['id'=> 'category_id' ])->viaTable('joke2category', ['joke_id' => 'id']);
     }
 
     
