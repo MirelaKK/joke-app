c1d0f0c918062d1c7d0173ea0a480849c05819a4
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-04 22:56:09 +0200

    Changed the title and breadcrumbs label

--- views/category/create.php
+++ views/category/create.php
@@ -6,8 +6,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Category */
 
-$this->title = 'Create Category';
-$this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
+$this->title = 'Dodaj kategoriju';
+$this->params['breadcrumbs'][] = ['label' => 'Kategorije', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="category-create">
