629239e892e3952733331114309b8158fb56b55f
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-08 00:04:57 +0200

    Added buttons for next and last joke update view

--- views/joke/update.php
+++ views/joke/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Joke */
 
-$this->title = 'Update-uj Vic: ' . $model->title;
+$this->title = 'Promijeni Vic: ' . $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'Vicevi', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
@@ -17,5 +17,8 @@ $this->params['breadcrumbs'][] = 'Update';
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
-
+    <p>
+    <?= Html::a('Prethodni', ['update', 'id' => ($model->id===1)?($model->id):($model->id -1)], ['class' => 'btn btn-default pull-left']) ?>
+    <?= Html::a('Naredni', ['update', 'id' => $model->id+1], ['class' => 'btn btn-default pull-right']) ?>
+    </p>
 </div>
