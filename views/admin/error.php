0b46d633d3b48b9d288c22a113da320a830497c5
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-07 22:39:56 +0200

    Changed the error message

--- views/admin/error.php
+++ views/admin/error.php
@@ -7,21 +7,17 @@
 
 use yii\helpers\Html;
 
-$this->title = $name;
+$this->title = 'Nije pronađena';
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <!--<h1><?= Html::encode($this->title) ?></h1> -->
 
     <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <?= nl2br(Html::encode('Stranica nije pronađena ili nemate pristup ovom dijelu.')) ?>
     </div>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+
+    
 
 </div>
