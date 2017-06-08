93acf27b792065079eda600f611c4f2396255dd5
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-08 17:24:17 +0200

    Modified the footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -94,7 +94,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left"> <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
