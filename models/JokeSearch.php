cf3472e3b90e80abe109f01316217b18b8112c53
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-09 10:42:40 +0200

    Added search for multiple categories

--- models/JokeSearch.php
+++ models/JokeSearch.php
@@ -7,6 +7,8 @@ use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Joke;
 use app\models\JokeWithCategory;
+use app\models\JokeCategory;
+use app\models\Category;
 
 /**
  * JokeSearch represents the model behind the search form about `app\models\Joke`.
@@ -36,7 +38,7 @@ class JokeSearch extends JokeWithCategory
      */
     public function search($params)
     {
-        $query = Joke::find()->where(['not', ['joke_status_id' => 5]]);
+        $query = JokeWithCategory::find()->where(['not', ['joke_status_id' => 5]]);
         
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
@@ -56,25 +58,38 @@ class JokeSearch extends JokeWithCategory
             'admin_id' => $this->admin_id,
             'joke_rating' => $this->joke_rating,
         ]);
+        
+        $ids = $this->getJokeIdsFromCategoryIds($params);
 
-        $category_ids=implode(' ', $this->category_ids);
-        $query->andFilterWhere(['like', 'title', $this->title])
+        $query->andFilterWhere(['in','id',$ids])
+            ->andFilterWhere(['like', 'title', $this->title])
             ->andFilterWhere(['like', 'joke', $this->joke])
             ->andFilterWhere(['like', 'submitter', $this->submitter])
             ->andFilterWhere(['like', 'approval_date', $this->approval_date])
             ->andFilterWhere(['like', 'submit_date', $this->submit_date])
             ->andFilterWhere(['like', 'publish_date', $this->publish_date])
             ->andFilterWhere(['like', 'joke_of_day_date', $this->joke_of_day_date]);
-        
-        if(null != $this->category_ids){ 
-            foreach ($params['category_ids'] as $value) {
-            foreach ($this->category_ids as $key => $category_id) {
-               
-                   $query->orFilterWhere(['like', $value, $category_id]); 
-                }
-                
-            }
-        }
+  
         return $dataProvider;
+    }
+    protected function getJokeIdsFromCategoryIds($params){
+               if(isset($params['JokeSearch']['category_ids'])){
+           $category_ids=[];
+       
+           foreach($params['JokeSearch']['category_ids'] as $category_id){
+
+               $category_ids[]=$category_id;
+           }
+           $subqueries= JokeCategory::find()
+                    ->select('joke_id')
+                   ->where(['in','category_id',$category_ids])
+                    ->all();
+            $ids=[];
+            foreach($subqueries as $subquery){
+                $ids[]=$subquery->joke_id;
+            }
+
+       return $ids;
+        }
     }
 }
