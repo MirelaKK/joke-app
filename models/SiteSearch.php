21f9576cbc9dc2cf5de09a196561a0c420ed29c1
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-11 17:20:00 +0200

    created search model for filtering only joke titles and texts

---
+++ models/SiteSearch.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Joke;
+use yii\helpers\ArrayHelper;
+
+/**
+ * SiteSearch represents the model behind the search form about `app\models\Joke`.
+ */
+class SiteSearch extends Joke
+{
+    public $search;
+    public function rules()
+    {
+        return [ 
+            [['search'], 'safe']];
+    }
+    
+        public function attributeLabels()
+    {
+        return ArrayHelper::merge(parent::attributeLabels(), [
+            'search' => 'Pretraga',
+        ]);
+    }
+
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    
+    {
+        $query = Joke::find()->where(['joke_status_id' => 4]);
+        
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort' =>['defaultOrder'=>['submit_date'=>SORT_ASC]]
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            return $dataProvider;
+        }
+         // grid filtering conditions 
+
+        $query->andFilterWhere(['like', 'title', $params['SiteSearch']['search']])
+            ->andFilterWhere(['like', 'title', $params['SiteSearch']['search']]);
+            
+        
+        return $dataProvider;
+    }
+
+}
