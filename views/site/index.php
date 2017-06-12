649a1aeb6daf26ad2a603a7d1530c03382cdf261
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-12 15:40:18 +0200

    Added some content

--- views/site/index.php
+++ views/site/index.php
@@ -2,9 +2,17 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'Vicevi';
+$this->title = 'Naslovna';
 ?>
 <div class="site-index">
-
+    <h3 style="color: black">Vic dana</h3>
+    <hr>
+   
+<?= $joke_of_day ?>
+    <br>
+    <br>
+    <h3 style="color: black">Najnoviji vicevi</h3>
+    <hr>
+<?= $new_jokes ?>
 
 </div>
