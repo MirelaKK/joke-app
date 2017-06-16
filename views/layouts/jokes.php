01a096b56c53b5116b4fb0c2bc4f22dcd2133056
DanijelaKraljevic <dannijella1123@gmail.com>
2017-06-16 22:35:33 +0200

    Optimized db query

--- views/layouts/jokes.php
+++ views/layouts/jokes.php
@@ -4,10 +4,7 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
 use yii\helpers\Url;
 use app\assets\MyAppAsset;
 use app\models\Category;
@@ -55,13 +52,12 @@ $model = new SiteSearch();
                       <?= Html::a('Najbolji vicevi', Url::to(['/site/best']),['class'=>'btn btn-outline-primary btn-sm']) ?>
                       <?= Html::a('Najnoviji vicevi', Url::to(['/site/new']),['class'=>'btn btn-outline-primary btn-sm']) ?>
                       <?php 
-                          $count=Category::find()->count();
+                          $categories=Category::find()->all();
 
-                          for($i=1;$i<=$count;$i++) { ?>
-                          <?php $query=Category::findOne($i);
-                          ?>
-                              <?= Html::a($query->category, Url::to(['/site/joke-category', 'id' => $i]),['class'=>'btn btn-outline-primary btn-sm']) ?>
-                            <?php   
+                          foreach($categories as $category) { 
+                          
+                              echo Html::a($category->category, Url::to(['/site/joke-category', 'id' => $category->id]),['class'=>'btn btn-outline-primary btn-sm']);
+                              
                           }
                       ?>
 
